8be47963fb43af3610dad7d8cad149abee9ec734
Xun <as20019821@gmail.com>
2022-08-08 01:45:13 +0800

    更改 mysqli_connect()

--- dbtools.inc.php
+++ dbtools.inc.php
@@ -6,6 +6,18 @@ function create_connection()
 
   mysqli_query($link, "SET NAMES utf8");
 
+  // 新增clearDBMSQL
+  //Get Heroku ClearDB connection information
+  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $cleardb_server = $cleardb_url["host"];
+  $cleardb_username = $cleardb_url["user"];
+  $cleardb_password = $cleardb_url["pass"];
+  $cleardb_db = substr($cleardb_url["path"], 1);
+  $active_group = 'default';
+  $query_builder = TRUE;
+  // Connect to DB
+  $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
   return $link;
 }
 
